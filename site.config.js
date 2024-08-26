6d06d00a250b81d46903c2f221b757dcc3346b8b
icarufc <joaoicaromoreira@gmail.com>
2024-08-26 18:13:57 -0300

    desativando utterances

--- site.config.js
+++ site.config.js
@@ -54,7 +54,7 @@ const CONFIG = {
     },
   },
   utterances: {
-    enable: true,
+    enable: false,
     config: {
       repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "https://github.com/soulcarus/soulcarus-dev-log",
       "issue-term": "og:title",
